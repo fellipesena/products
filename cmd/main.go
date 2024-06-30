3a2848330d36dd19b6010cf2784f36d9310a6e33
Fellipe Sena <pfosena@gmail.com>
2024-06-30 15:50:06 -0300

    add migrations

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
+	runMigration()
+
 	server := gin.Default()
 
 	server.GET("/teste", func(ctx *gin.Context) {
@@ -16,4 +22,30 @@ func main() {
 	})
 
 	server.Run(":8080")
+}
+
+func runMigration(){
+	db, err := sql.Open("postgres", "postgres://admin:admin@localhost:5432/products?sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"products", driver)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		panic(err)
+	}
 }
